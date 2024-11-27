42f7399e75f9b7f158859a085992a1faabbfd644
Lorraine Rojas <ljrojasp@unal.edu.co>
2024-11-26 21:30:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,68 @@ import unicodedata
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 
+etiquetas = {
+    0: "ABUSO DE CONFIANZA",
+    1: "ACCIDENTE AERONAVE",
+    2: "ACCIDENTE DE TRANSITO",
+    3: "ACTOS MAL INTENCIONADOS DE TERCEROS",
+    4: "ANEGACION",
+    5: "AUMENTO DE TEMPERATURA",
+    6: "AVERIA",
+    7: "CAIDA DE OBJETOS EXTRAÑOS",
+    8: "CAMBIOS DE VOLTAJE",
+    9: "CORTO CIRCUITO",
+    10: "CULPA EXCLUSIVA DE LA VICTIMA",
+    11: "DAÑO A LA MERCANCÍA",
+    12: "DAÑO INTERNO",
+    13: "DAÑOS",
+    14: "DAÑOS ELECTRICOS",
+    15: "DAÑOS POR AGUA",
+    16: "DESCUIDO IMPERICIA Y NEGLIGENCIA",
+    17: "DESMEMBRACIÓN",
+    18: "ENFERMEDAD",
+    19: "EQUIPO ELECTRONICO",
+    20: "ERROR D PRESTACIÓN  D SERV. PROFESIONAL",
+    21: "EXPLOSIÓN",
+    22: "EXTRAVIO",
+    23: "FALTA DE ENTREGA TOTAL O PARCIAL",
+    24: "FENOMENO NATURAL",
+    25: "GASTOS ADICIONALES",
+    26: "GASTOS DE DEFENSA",
+    27: "GOLPE",
+    28: "GRANIZADA",
+    29: "GUERRA",
+    30: "HURTO CALIFICADO",
+    31: "HURTO SIMPLE",
+    32: "IMPERICIA DE OPERADORES O TRABAJADORES",
+    33: "IMPERICIA DESCUIDO",
+    34: "IMPERICIA EN EL MANEJO",
+    35: "INCAPACIDAD TOTAL O PERMANENTE",
+    36: "INCENDIO",
+    37: "INCUMPLIMIENTO DE ENTREGAS PARCIALES",
+    38: "INCUMPLIMIENTO DEL CRONOGRAMA",
+    39: "INCUMPLIMIENTO DEL PAGO DE SALARIOS",
+    40: "INUNDACION",
+    41: "INVESTIGACIÓN DISCIPLINARIA",
+    42: "LESIONES PERSONALES",
+    43: "MUERTE ACCIDENTAL",
+    44: "MUERTE CON VIOLENCIA",
+    45: "MUERTE NATURAL",
+    46: "OPERACIONES DE CARGUE Y DESCARGUE",
+    47: "PERDIDA DE REFRIGERACIÓN",
+    48: "PREDIOS, LABORES Y OPERACIONES",
+    49: "PROCESO FISCAL",
+    50: "RAYO",
+    51: "ROTURA DE MAQUINARIA",
+    52: "ROTURA DE TUBO",
+    53: "SAQUEO",
+    54: "SUICIDIO",
+    55: "TERREMOTO TEMBLO Y/O ERUPCIÓN VOLVANICA",
+    56: "VANDALISMO",
+    57: "VARIACION DE VOLTAJE",
+    58: "VIENTOS FUERTES"
+}
+
 # Cargar modelo y vocabulario
 @st.cache_resource
 def load_model_and_vocab():
@@ -51,7 +113,7 @@ def predecir_causas(dataframe, modelo):
     secuencias = tf.convert_to_tensor(textos_preprocesados, dtype=tf.string)
     predicciones = modelo.predict(secuencias)
     causas = predicciones.argmax(axis=1)
-    return causas
+    return [etiquetas[c] for c in causas]
 
 # Interfaz Streamlit
 st.title("Clasificación de Siniestros Viales")
